538fcf25a5e8f7e62ba09378c7ffe96636704e41
Rigil <alok1@rigil.in>
2017-06-30 13:22:26 +0530

    Data commit

--- EDairy/ViewController.swift
+++ EDairy/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CapturedImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnScan(_ sender: Any) {
+    }
 
 }
 
